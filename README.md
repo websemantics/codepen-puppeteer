b98545ca85388a43296d732466c0699c37ba582b
Adnan <adnan@websemantics.ca>
2017-09-12 12:23:28 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# codepen-puppeteer
+```
+╭─╮   ╭─╮   ╭┬╮   ╭─╮   ╭─╮   ╭─╮   ╭╮╭       ┬   ╭─╮
+│     │ │    ││   ├┤    ├─╯   ├┤    │││       │   │ │
+╰─╯   ╰─╯   ─┴╯   ╰─╯   ┴     ╰─╯   ╯╰╯   o   ┴   ╰─╯
+╭────╮╭──╮╭╮╭────╮╭────╮╭────╮ ╭──╮ ╭────╮╭────╮╭─┬─╮                 
+│  ╭╮││  ││││  ╭╮││  ╭╮││ ─  │╭╯  ╰╮│ ─  ││ ─  ││   │                 
+│  ╰╯││  ╰╯││  ╰╯││  ╰╯││    │╰╮  ╭╯│    ││    ││  ╭╯                 
+│   ╭╯│    ││   ╭╯│   ╭╯│ ───┤ │ ─┤ │ ───┤│ ───┤│  │                  
+╰───╯ ╰───┴╯╰───╯ ╰───╯ ╰────╯ ╰──╯ ╰────╯╰────╯╰──╯                  
+```
+> Use Puppeteer to download pens from Codepen.io as single html pages.
+
+## Features
+
+- Download pens as a single html page
+- Easy preview with index page
+- Built-in error recovery to resume download
+- Skip already downloaded pens
+- Easy to debug using screenshots
+- Custom template pages
+- Easy to follow commented source code with comments
+
+## Usage
+
+- Install dependencies (`puppeteer`),
+
+```bash
+npm i
+```
+
+- Change search query @ `./index.js` line `#14`
+
+- Start download
+
+```bash
+npm run download
+```
+
+- Browse to `./pens/index.html` to preview full list of downloads
+
+## Debug
+
+This project is a proof of concept so you might find problematic pens that wouldn't download fully. Turn the debug flag on (`debug = true`) @ `./index.js` line `#13` to enable screenshots which might help you debug the issue.
+
+## Hint
+
+I find the following command useful to force quit running `chromium` processes on OSX
+
+```
+pkill -f -- "chromium"
+```
+
+## Preview
+
+<img alt="Codepen Puppeteer Preview Page" width="850" src="assets/img/preview.gif" />
+
+## Resources
+
+- [Puppeteer](https://github.com/GoogleChrome/puppeteer)
+- [Getting started with Puppeteer and Chrome Headless for Web Scraping](https://medium.com/@e_mad_ehsan/getting-started-with-puppeteer-and-chrome-headless-for-web-scrapping-6bf5979dee3e)
+
+## Support
+
+Need help or have a question? post at [StackOverflow](https://stackoverflow.com/questions/tagged/codepen-puppeteer+websemantics).
+
+*Please don't use the issue trackers for support/questions.*
+
+*Star if you find this project useful, to show support or simply for being awesome :)*
+
+## Contribution
+
+Contributions to this project are accepted in the form of feedback, bugs reports and even better - pull requests.
+
+## License
+
+[MIT license](http://opensource.org/licenses/mit-license.php) Copyright (c) Web Semantics, Inc.
---
+++ index.js
@@ -0,0 +1,193 @@
+/**
+ * @copyright (c) 2016-2017, Web Semantics, Inc.
+ * @author Adnan M.Sagar, PhD. <adnan@websemantics.ca>
+ * @license Distributed under the terms of the MIT License.
+ */
+
+var puppeteer = require('puppeteer')
+var querystring = require('querystring')
+var fs = require('fs')
+
+/* Application parameters : change as appropriate */
+
+var debug = false
+var searchQuery = 'flexbox'
+var pensPath = 'pens/'
+var debugPath = 'debug/'
+
+/**
+ * Download Codepen pages
+ * 
+ * @param {String} penTemplate - pen html template (find @ `./assets/templates/pen.html`)
+ * @param {String} indexTemplate - index page html template (find @ `./assets/templates/index.html`)
+ * @private
+ */
+async function download(penTemplate, indexTemplate) {
+  var browser = await puppeteer.launch()
+  var page = await browser.newPage()
+  var searchUrl = 'https://codepen.io/search/pens'
+  var params = {limit: 'all', q: searchQuery}
+  var startPage = 1
+  var endPage = 100
+  var pens = []
+  var links = null
+
+  if (debug) {
+    await page.setViewport({width: 1400, height: 800})
+  }
+
+  /* (1) Request result page from `startPage` to `endPage` */
+  for (params.page = startPage; params.page <= endPage; params.page++) {
+    /* (2) Get pens urls from search page (unless in debug mode) */
+    if (!debug) {
+      log('Search for "' + params.q + '", PAGE #' + params.page)
+
+      await page.goto(searchUrl + '/?' + querystring.stringify(params), {
+        waitUntil: 'load'
+      })
+
+      links = await page.evaluate(() => {
+        var list = document.querySelectorAll('.meta > .item-title > a')
+        var links = []
+
+        for (var element of list) {
+          links.push({
+            title: element.innerHTML,
+            url: element.getAttribute('href')
+          })
+        }
+
+        return links
+      })
+    } else {
+      /* Replace url with problematic pen (debug mode) */
+      links = [
+        {
+          url: 'https://codepen.io/osj2507/pen/ZYBBpw',
+          title: 'Flexbox Masonry'
+        }
+      ]
+    }
+
+    /* (3) Request individual pens */
+    for (var pen of links) {
+      var filename = titleToFilename(pen.title) + '.html'
+
+      /* (3.a) Add link to pens collection */
+      pens.push(pen)
+
+      /* (3.b) Skip download if pen already exists  */
+      if (fs.existsSync(pensPath + filename)) {
+        log('Pen "' + pensPath + filename + '" already saved!')
+        continue
+      }
+
+      log('Processing "' + pen.title.trim() + '" @ ' + pen.url)
+
+      await page.goto(pen.url, {waitUntil: 'networkidle'})
+
+      if (debug) {
+        /* save page before assets are compiled */
+        await page.screenshot({path: debugPath + 'before.png', fullPage: true})
+      }
+
+      /* (3.c) Wait until the iframe is loded */
+      await page.waitForSelector('.result-iframe[src^="https"]')
+
+      /* (3.d) Click Code Box dropdown then complie link (html, css and javascript ) */
+      await page.click('#box-html .editor-actions-right > button')
+      await page.click('#html-view-compiled')
+
+      await page.click('#box-css .editor-actions-right > button')
+      await page.click('#css-view-compiled')
+
+      await page.click('#box-js .editor-actions-right > button')
+      await page.click('#js-view-compiled')
+
+      /* (3.e) Get content from CodeMirror */
+      var content = await page.evaluate(() => {
+        var list = document.querySelectorAll('.code-wrap .CodeMirror')
+        var content = []
+
+        for (var editor of list) {
+          content.push(editor.CodeMirror.getValue())
+        }
+
+        return content
+      })
+
+      if (debug) {
+        /* save page after assets are compiled to detect any issues */
+        await page.screenshot({path: debugPath + 'after.png', fullPage: true})
+      }
+
+      /* (3.f) Construct an html page from the pen template */
+      var html = penTemplate
+        .replace('{{html}}', content[0] ? content[0] : '')
+        .replace('{{title}}', pen.title.trim())
+        .replace('{{url}}', pen.url)
+        .replace('{{style}}', content[1] ? content[1] : '')
+        .replace('{{javascript}}', content[2] ? content[2] : '')
+
+      fs.writeFileSync(pensPath + filename, html)
+      log('... saved to "' + pensPath + filename + '"')
+    }
+
+    /* (4) Finally, save an index.html page with the list of downloaded pens (so far) */
+
+    var list = ''
+
+    for (var pen of pens) {
+      var title = pen.title.trim()
+      list +=
+        '<a href="' +
+        titleToFilename(title) +
+        '.html" target="iframe">' +
+        title +
+        '</a>'
+    }
+
+    fs.writeFileSync(
+      pensPath + 'index.html',
+      indexTemplate.replace('{{list}}', list)
+    )
+  }
+
+  browser.close()
+}
+
+/**
+ * Clean text to be used as filename
+ * 
+ * @param {String} string - page title
+ * @private
+ */
+function titleToFilename(string) {
+  return string
+    .trim()
+    .replace(/[^a-z0-9]/gi, '-')
+    .replace(/-+/g, '-')
+    .replace(/-$|^-/g, '')
+    .toLowerCase()
+    .trim()
+}
+
+/**
+ * Log to console
+ * 
+ * @param {String} string - output string
+ * @private
+ */
+function log(string) {
+  console.log(string)
+}
+
+/* Ensure folders exist */
+if (!fs.existsSync(debugPath)) { fs.mkdirSync(debugPath) }
+if (!fs.existsSync(pensPath)) { fs.mkdirSync(pensPath) }
+
+/* Read html template and execute download function */
+download(
+  fs.readFileSync('assets/templates/pen.html', {encoding: 'utf8'}),
+  fs.readFileSync('assets/templates/index.html', {encoding: 'utf8'})
+)
